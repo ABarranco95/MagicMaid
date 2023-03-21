60b90be99fdace158c504141638b0f5e70b0ca5b
abarranco95 <abarranco650@gmail.com>
2023-03-21 12:52:08 -0700

    Initial commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to the Magic Maid</h1>
+    </div>
+  );
+};
+
+export default HomePage;
