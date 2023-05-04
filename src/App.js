30167fbdd9b7ad3f348ced8c08a8011bb85f2deb
abarranco95 <abarranco650@gmail.com>
2023-05-04 16:26:49 -0700

    added new font

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage';
 import CleaningsPage from './pages/CleaningsPage/CleaningsPage';
-import StandardCleanPage from './pages/CleaningsPage/StandardCleanPage/StandardCleanPage.jsx';
-import DeepCleanPage from './pages/CleaningsPage/DeepCleanPage/DeepCleanPage.jsx';
-import MovingCleanPage from './pages/CleaningsPage/MovingCleanPage/MovingCleanPage';
 import LeadForm from './components/LeadForm/LeadForm';
 import Footer from './components/Footer/Footer';
 import BookingForm from './components/BookingForm/BookingForm';
 import GiftCards from './components/GiftCard/GiftCards';
 
+// Import the font
+import '@fontsource/roboto';
+
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/quote-form" element={<LeadForm /> }  />
-        <Route path="/booking-form" element={<BookingForm /> }  />
-        <Route path="/cleanings" element={<CleaningsPage />} />
-        <Route path="/gift-cards" element={<GiftCards />} />
-        <Route path="/lead-form" element={<LeadForm />} />
-      </Routes>
-      <Footer />
+      <div className="App" style={{ fontFamily: 'Roboto' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quote-form" element={<LeadForm /> }  />
+          <Route path="/booking-form" element={<BookingForm /> }  />
+          <Route path="/cleanings" element={<CleaningsPage />} />
+          <Route path="/gift-cards" element={<GiftCards />} />
+          <Route path="/lead-form" element={<LeadForm />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 };
